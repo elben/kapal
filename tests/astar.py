9b0dc2d89368d28dbbb9d87ad76022dd0675b9f9
Elben Shira <elbenshira@gmail.com>
2009-07-25 23:15:43 -0500

    fixed bugs on experimental set up

--- tests/astar.py
+++ tests/astar.py
@@ -17,22 +17,22 @@ def main():
     width = 50
     max_width = 300
     x_axis = []
-    time = []
+    y_axis = []
     while width < max_width:
         print width
 
-        c = tools.rand_cost_map(250, 250, 1, 3, flip=True)
+        c = tools.rand_cost_map(width, width, 1, 3, flip=True)
         w = World2d(c, state_type = State2dAStar)
 
-        astar = AStar(w, w.state(0,0), w.state(99, 99))
+        astar = AStar(w, w.state(0,0), w.state(width-1, width-1))
         start_time = time.time()
         astar.plan()
-        time.append(time.time() - start_time)
+        y_axis.append(time.time() - start_time)
 
         path = astar.path()
         x_axis.append(width)
 
         width += 50
     print x_axis
-    print time
+    print y_axis
 main()
