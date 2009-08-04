eacc42c3a0cdbe0d7576101dd6d87a46dc85ae52
Elben Shira <elbenshira@gmail.com>
2009-08-03 21:55:31 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='kapal',
+      version='0.1.0',
+      author='Elben Shira',
+      author_email='elbenshira@gmail.com',
+      url='kapal.googlecode.com',
+      description='Path-planning library.',
+      packages=['kapal'],
+      )
+
