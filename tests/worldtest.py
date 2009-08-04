868b1c825410fb88485e27a893325531920a09df
Elben Shira <elbenshira@gmail.com>
2009-08-03 21:53:01 -0500

    using unittests

--- tests/worldtest.py
+++ tests/worldtest.py
@@ -1,45 +1,48 @@
 import sys
 import os
-sys.path.append(os.path.join(os.getcwd(), '..'))
-from world import *
+import unittest
+from kapal.world import *
 
-def main():
-    c =  [[0, 1, 2],
-          [3, 4, 5],
-          [6, 7, 8],
-          [9, 1, 2]]
-    w = World2d(c, state_type = State2dAStar)
+class TestWorld2d(unittest.TestCase):
+    def setUp(self):
+        self.c =  [[0, 1, 2],
+                   [3, 4, 5],
+                   [6, 7, 8],
+                   [9, 1, 2]]
+        self.w = World2d(self.c, state_type=State2dAStar)
 
     ######  Test 1  ######
     # successors
-    s = w.state(2, 1)
-    c1 = [4, 6, 8, 1]
-    c2 = []
-    for i, j in w.succ(s):
-        c2.append(j)
-    if c1 != c2:
-        print "1: Failed."
+    def testSucc1(self):
+        s = self.w.state(2, 1)
+        c1 = [4, 6, 8, 1]
+        c2 = []
+        for i, j in self.w.succ(s):
+            c2.append(j)
+        self.assertEqual(c1, c2)
 
     ######  Test 2  ######
     # predecessors
-    s = w.state(0, 0)
-    c1 = [1, 3]
-    c2 = []
-    for i, j in w.pred(s):
-        c2.append(j)
-    if c1 != c2:
-        print "2: Failed."
+    def testPred1(self):
+        s = self.w.state(0, 0)
+        c1 = [1, 3]
+        c2 = []
+        for i, j in self.w.pred(s):
+            c2.append(j)
+        self.assertEqual(c1, c2)
 
     ######  Test 3  ######
     # change cost
-    s1 = w.state(0, 0)
-    s2 = w.state(0, 1)
-    w.change_c(s1, s2, 100)
-    c1 = [100, 3]
-    c2 = []
-    for i, j in w.pred(s):
-        c2.append(j)
-    if c1 != c2:
-        print "3: Failed."
+    def testChangeCost1(self):
+        s1 = self.w.state(0, 0)
+        s2 = self.w.state(0, 1)
+        self.w.change_c(s1, s2, 100)
+        c1 = [100, 3]
+        c2 = []
+        for i, j in self.w.pred(s1):
+            c2.append(j)
+        self.assertEqual(c1, c2)
 
-main()
+if __name__ == '__main__':
+    unittest.main()
+
